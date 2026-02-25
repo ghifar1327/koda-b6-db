530af69aba18784f7676289afbde059b8a250760
ghifar1327 <givartorreto@gmail.com>
2026-02-25 13:14:15 +0700

    fix; query product for sslect size and variant

--- query.sql
+++ query.sql
@@ -1,17 +1,21 @@
--- query data product
+-- query 1 product dengan melih varian dan size lalu menghitung final price
 SELECT 
-    p.name, 
-    p.price, 
-    STRING_AGG(DISTINCT s.name, ', ') AS sizes,
-    STRING_AGG(DISTINCT v.name, ', ') AS variants
+    p.name,
+    p.price AS base_price,
+    s.name AS size,
+    s.add_price AS size_price,
+    v.name AS variant,
+    v.add_price AS variant_price,
+    (p.price + s.add_price + v.add_price) AS total_price
 FROM products p
-JOIN product_sizes ps
-    on ps.product_id = p.id
-JOIN sizes s
-    on s.id = ps.size_id
-JOIN product_variant pv
-    on pv.product_id = p.id
+JOIN product_sizes ps 
+    ON ps.product_id = p.id
+JOIN sizes s 
+    ON s.id = ps.size_id
+JOIN product_variant pv 
+    ON pv.product_id = p.id
 JOIN variants v 
-    on v.id = pv.variant_id
-GROUP BY p.id
-
+    ON v.id = pv.variant_id
+WHERE p.id = 9
+AND s.id = 2  
+AND v.id = 4; 
